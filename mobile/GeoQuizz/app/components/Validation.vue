82f5558fd5702e7db2aa0fb3c717dee01ce5d162
Guebel Marc <marc.guebel@sfr.fr>
2019-03-14 09:56:58 +0100

    mobile : on avance

--- mobile/GeoQuizz/app/components/Validation.vue
+++ mobile/GeoQuizz/app/components/Validation.vue
@@ -1,19 +1,21 @@
 <template>
     <Page>
     <ActionBar title="My App">
-        <NavigationButton text="Go back" android.systemIcon="ic_menu_back" @tap="changeRoute('Position')" />
+        <NavigationButton text="Go b" android.systemIcon="ic_menu_back" @tap="changeRoute('Position')" />
     </ActionBar>
-    <GridLayout class="weather-box" columns="1*,6*,1*" rows="auto">
-             <StackLayout col="1" row="0">
-        <label>{{lat}}</label><label>{{long}}</label>
-        <Image :src="img" height="10%"/>
-        
-    </StackLayout></GridLayout>
+    <FlexboxLayout flexDirection="column">
+
+        <label>{{lat}}</label><label>{{long}}</label><label>{{img64}}</label>
+        <Image id="preview" :src="img" style="height:70%"/>
+        <button text="Valider" @tap="validation"/>
+    </FlexboxLayout>
     </Page>
     
 </template>
 
 <script>
+    import axios from 'axios';
+    var base64Img = require('base64-img');
     let LS = require( "nativescript-localstorage" );
     export default {
         data() {
@@ -21,14 +23,31 @@
                 img: LS.getItem('img'),
                 lat: LS.getItem('lat'),
                 long: LS.getItem('long'),
+                img64: '',
             }
         },
         methods: {
             changeRoute(to){
                 this.$navigateTo(this.$routes[to], { clearHistory: true});
             },
+            validation(){
+                /*let axiosConfig = {
+                  headers: {
+                      'Authorization': 'Client-ID c0c7cb0b6d1041e',
+                      'Authorization': 'Bearer 26981291d8de19e64af6cba81ce4bcf65476e29e',
+                  }
+                };
+                axios.post('https://api.imgur.com/3/image', this.img64, axiosConfig)
+                .then((res) => {
+                  console.log("RESPONSE RECEIVED: ", res);
+                })
+                .catch((err) => {
+                  console.log("AXIOS ERROR: ", err);
+                })*/
+            },
         },
         mounted() {
+            this.img64 = base64Img.base64(this.img, function(err, data) {})
         }
     }
 </script>
--- mobile/GeoQuizz/app/main.js
+++ mobile/GeoQuizz/app/main.js
@@ -3,6 +3,7 @@ import Picture from './components/Picture'
 import VueDevtools from 'nativescript-vue-devtools'
 import routes from './router'
 import Mapbox from 'mapbox-gl-vue';
+import axios from 'axios';
 var geolocation = require("nativescript-geolocation");
 
 require( "nativescript-localstorage" );
@@ -18,6 +19,17 @@ Vue.prototype.$routes = routes;
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
+
+/*window.axios = axios.create({
+	baseURL: '',
+	params: {
+		token : false
+	},
+	headers : {
+		Authorization: 'Client-ID c0c7cb0b6d1041e',
+        Authorization: 'Bearer 26981291d8de19e64af6cba81ce4bcf65476e29e'
+	}
+});*/
 
 new Vue({
   render: h => h('frame', [h(Picture)])
