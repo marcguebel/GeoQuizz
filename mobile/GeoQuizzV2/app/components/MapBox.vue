feaa83975b7fe1bd38d341844e8cad35ea06fd33
Guebel Marc <marc.guebel@sfr.fr>
2019-03-20 13:51:30 +0100

    Fin de l'application mobile
    Normalement l'application est terminé (j'ai bien dit normalement)

--- mobile/GeoQuizzV2/app/components/MapBox.vue
+++ mobile/GeoQuizzV2/app/components/MapBox.vue
@@ -1,74 +1,72 @@
 <template>
-    <Page class="page">
-        <ActionBar class="action-bar" title="Map">
-            <NavigationButton text="Retour" android.systemIcon="ic_menu_back" @tap="changeRoute('Position')" />
-        </ActionBar>
-        <GridLayout >
-            <Mapbox
-                accessToken="pk.eyJ1IjoibWFyY2d1ZWJlbCIsImEiOiJjanQ0ZWJpcXYxMDB3M3lwZ3U0YjZ3emliIn0.blWnH7mREgxkPfuOcA87zA"  
-                mapStyle="dark"
-                latitude="48.682968"
-                longitude="6.161096"
-                hideCompass="false"
-                zoomLevel="12"
-                showUserLocation="false"
-                disableZoom="false"
-                disableRotation="true"
-                disableScroll="false"
-                disableTilt="false"
-                @mapReady="onMapReady($event)"
-                @onTap="addMarker()"
-                ref="map">
-            </Mapbox>
-        </GridLayout>
-    </Page>
+	<Page class="page">
+		<ActionBar class="action-bar" title="Map">
+			<NavigationButton android.systemIcon="ic_menu_back" @tap="changeRoute('Position')" />
+		</ActionBar>
+		<GridLayout >
+			<Mapbox
+				accessToken="pk.eyJ1IjoibWFyY2d1ZWJlbCIsImEiOiJjanQ0ZWJpcXYxMDB3M3lwZ3U0YjZ3emliIn0.blWnH7mREgxkPfuOcA87zA"  
+				mapStyle="dark"
+				latitude="48.682968"
+				longitude="6.161096"
+				hideCompass="false"
+				zoomLevel="12"
+				showUserLocation="false"
+				disableZoom="false"
+				disableRotation="true"
+				disableScroll="false"
+				disableTilt="false"
+				@mapReady="onMapReady($event)"
+				@tap="addMarker()"
+				ref="map">
+			</Mapbox>
+		</GridLayout>
+	</Page>
 </template>
 
 <script>
-    let LS = require( "nativescript-localstorage" );
-    import * as utils from "utils/utils";
-    import Mapbox from 'mapbox-gl-vue';
-    export default {
-        data () {
-            return { };
-        },
-        methods: {
-            onMapReady(args) { // Function appelé au chargement de la map
-                console.log(args)
-                /*var vm = this;
-                setTimeout(function () {
-                    vm.addMarker();
-                },5000);*/
-            },
-            addMarker(){ // Ajout d'un marker sur la map
-                /*this.$refs.map.nativeView.addMarkers([
-                    {
-                        lat: 48.682968,
-                        lng: 6.161096,
-                        title: 'One-line title here',
-                        subtitle: 'Really really nice location',
-                        selected: true, // makes the callout show immediately when the marker is added (note: only 1 marker can be selected at a time)
-                        onCalloutTap: function(){console.log("'Nice location' marker callout tapped");}
-                    }]
-                );*/
-            },
-            changeRoute(to){this.$navigateTo(this.$routes[to], { clearHistory: true});},
-        },
-        mounted(){
-        }
-    };
+	let LS = require( "nativescript-localstorage" );
+	import * as utils from "utils/utils";
+	export default {
+		data () {
+			return { };
+		},
+		methods: {
+			onMapReady(args) { // Function appelé au chargement de la map
+				console.log(args)
+				/*var vm = this;
+				setTimeout(function () {
+					vm.addMarker();
+				},5000);*/
+			},
+			addMarker(){ // Ajout d'un marker sur la map
+			console.log('taped')
+				/*this.$refs.map.nativeView.addMarkers([
+					{
+						lat: 48.682968,
+						lng: 6.161096,
+						title: 'One-line title here',
+						subtitle: 'Really really nice location',
+						selected: true, // makes the callout show immediately when the marker is added (note: only 1 marker can be selected at a time)
+						onCalloutTap: function(){console.log("'Nice location' marker callout tapped");}
+					}]
+				);*/
+			},
+			changeRoute(to){this.$navigateTo(this.$routes[to], { clearHistory: true});},
+		},
+	};
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
-
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+	ActionBar {
+		background-color: #53ba82;
+		color: #ffffff;
+	}
+	.message {
+		vertical-align: center;
+		text-align: center;
+		font-size: 20;
+		color: #333333;
+	}
+	#mapid { height: 180px; }
 </style>
--- mobile/GeoQuizzV2/app/components/Validation.vue
+++ mobile/GeoQuizzV2/app/components/Validation.vue
@@ -1,75 +1,83 @@
 <template>
-    <Page>
-        <ActionBar :title="'Bienvenue ' + userLog">
-            <NavigationButton text="Retou" android.systemIcon="ic_menu_back" @tap="changeRoute('Position')" />
-            <ActionItem @tap="deco" ios.systemIcon="9" ios.position="left" android.systemIcon="ic_delete" android.position="actionBar"></ActionItem>
-        </ActionBar>
-        <FlexboxLayout flexDirection="column">
-            <Progress id="pB" :value="currentProgress" />
-            <Label>Latitude : {{lat}}</Label>
-            <Label>Longitude : {{long}}</Label>
-            <Image id="preview" :src="img" style="height:70%"/>
-            <button id="btnV" text="Valider" @tap="validation"/>
-        </FlexboxLayout>
-    </Page>
+	<Page>
+		<ActionBar :title="'Bienvenue ' + userLog">
+			<NavigationButton android.systemIcon="ic_menu_back" @tap="changeRoute('Position')" />
+			<ActionItem @tap="changeRoute('Login')" ios.systemIcon="9" ios.position="left" android.systemIcon="ic_lock_power_off" android.position="actionBar"></ActionItem>
+		</ActionBar>
+		<FlexboxLayout flexDirection="column">
+			<Progress v-if="currentProgress != 0" id="pB" :value="currentProgress" />
+			<Label>Latitude : {{lat}}</Label>
+			<Label>Longitude : {{long}}</Label>
+			<Image id="preview" :src="img" style="height:70%"/>
+			<StackLayout orientation="horizontal" @tap="validation" id="btnV" class="btn">
+				<Label width="80%">VALIDER</Label>
+				<Image src="~/assets/images/check.png" height="90%" />
+			</StackLayout>
+		</FlexboxLayout>
+	</Page>
 </template>
 
 <script>
-    import axios from 'axios';
-    let LS = require( "nativescript-localstorage" );
-    export default {
-        data() {
-            return {
-                img : LS.getItem('img'),
-                img64 : LS.getItem('img64'),
-                lat : parseFloat(LS.getItem('lat')),
-                long : parseFloat(LS.getItem('long')),
-                userId : LS.getItem('userId'),
-                userLog : LS.getItem('userLog'),
-                currentProgress : 0
-            }
-        },
-        methods: {
-            validation(){
-                this.currentProgress = 10
-                var bodyFormData = new FormData();
-                bodyFormData.append("file", this.img64)
-                bodyFormData.append("upload_preset", "rw2vmwqe")
-                this.currentProgress = 20
-                axios.post("https://api.cloudinary.com/v1_1/dvxlz9kaz/image/upload", bodyFormData)
-                .then(res => {
-                    this.currentProgress = 50
-                    let urlImg = res.data.secure_url
-                    this.currentProgress = 70
-                    axios.post("https://backend-lmaillard.pagekite.me/photos", {
-                        latitude: this.lat,
-                        longitude: this.long,
-                        url: urlImg,
-                        idUser: this.userId
-                    })
-                    .then(res => {
-                        this.currentProgress = 100
-                        console.log ('ok')
-                        this.changeRoute('End')
-                    })
-                    .catch((e)=>{
-                        this.currentProgress = 0
-                        console.log('error :/')
-                    })
-                })
-                .catch((e)=>{
-                    console.log(e.response)
-                })
-            },
-            changeRoute(to){this.$navigateTo(this.$routes[to], { clearHistory: true});},
-            deco(){confirm({message: "Se déconnecter ?",okButtonText: "Confirmer",cancelButtonText: "Annuler"}).then(result => {if(result == true){this.changeRoute('Login')}});}
-        },
-        mounted() {
-        }
-    }
+	import axios from 'axios';
+	let LS = require( "nativescript-localstorage" );
+	export default {
+		data() {
+			return {
+				img : LS.getItem('img'),
+				img64 : LS.getItem('img64'),
+				lat : parseFloat(LS.getItem('lat')),
+				long : parseFloat(LS.getItem('long')),
+				userLog : LS.getItem('userLog'),
+				userToken : LS.getItem('userToken'),
+				currentProgress : 0
+			}
+		},
+		methods: {
+			/*
+			Envoir de l'image sur l'api:
+			->Envoie de l'image en base 64 sur l'api de cloudinary, retourne un lien.
+			->Enregistre le lien retourner et les coordonnées GPS de l'image dans l'api du backoffice.
+			*/
+			validation(){
+				this.currentProgress = 10
+				var bodyFormData = new FormData();
+				bodyFormData.append("file", this.img64)
+				bodyFormData.append("upload_preset", "rw2vmwqe")
+				this.currentProgress = 20
+				axios.post("https://api.cloudinary.com/v1_1/dvxlz9kaz/image/upload", bodyFormData)
+				.then(res => {
+					this.currentProgress = 50
+					let urlImg = res.data.secure_url
+					this.currentProgress = 70
+					axios.post("https://backend-lmaillard.pagekite.me/photos", {
+						latitude: this.lat,
+						longitude: this.long,
+						url: urlImg,
+					}, {
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': this.userToken,
+						}
+					})
+					.then(res => {
+						this.currentProgress = 100
+						this.changeRoute('End')
+					})
+					.catch((e)=>{
+						this.currentProgress = 0
+						console.log('error ' + e)
+					})
+				})
+				.catch((e)=>{
+					console.log('error ' + e)
+				})
+			},
+			changeRoute(to){this.$navigateTo(this.$routes[to], { clearHistory: true});},
+		},
+	}
 </script>
    
 <style scoped>
-    #preview, #btnV{ margin-top: 50px }
-    #pb {background-color:50px;}
+	#preview, #btnV{ margin-top: 50px }
+	#pb {background-color:50px; }
 </style>
